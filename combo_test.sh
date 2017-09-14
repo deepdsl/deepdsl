032238334e07db8dc6d2fd959c56b60fe643d087
pir-dsl <matthewhuang2004@gmail.com>
2017-09-14 01:40:08 -0500

    Remove bad param

--- combo_test.sh
+++ combo_test.sh
@@ -11,12 +11,12 @@ do_test() {
         do
         #test one network at a time
         nvidia-smi --query-gpu=memory.used --format=csv >> $network-$test_num.txt -lms $1 &
-        start=`date +%s.%N`
+        start=`date +%s`
         mvn -Plinux64 exec:java -Dexec.mainClass="$2.$network"
-        end=`date +%s.%N`
+        end=`date +%s`
         runtime=$((end-start))
         echo "$network execution time: $runtime" >> deepdsl-java/$result_file
-
+        
         #to use both $network and $1, need 1. use double quate and put backslash in front of $1 to allow
         #variable interpolation
         #kill $(jobs -p)
@@ -31,31 +31,31 @@ clean() {
        do
        	rm deepdsl-java/src/main/java/deepdsl/gen/$network.java 
        	rm deepdsl-java/src/main/java/deepdsl/gen/$network\_infer.java 
-       	rm deepdsl-java/$nextwork-test1.txt
-       	rm deepdsl-java/$nextwork-test2.txt
-       	rm deepdsl-java/$nextwork-test3.txt
+       	rm deepdsl-java/$network-test1.txt
+       	rm deepdsl-java/$network-test2.txt
+       	rm deepdsl-java/$network-test3.txt
        done
 }
 echo "Start testing"
 echo "Start test time $(date)" >> deepdsl-java/$result_file
 #assuming start from deepdsl root folder
 #Step 1: generate the code and compile
-start=`date +%s.%N`
+start=`date +%s`
 mvn -Plinux64 clean install -DskipTests 2>&1 >/dev/null
-end=`date +%s.%N`
+end=`date +%s`
 runtime=$((end-start))
 echo "maven build time: $runtime" >> deepdsl-java/$result_file
 #cd deepdsl-java
 #mvn -Plinux64 test -Dtest=TestNetwork#testAll
-start=`date +%s.%N`
+start=`date +%s`
 mvn -Plinux64 test -Dtest=TestNetwork 2>&1 >/dev/null
-end=`date +%s.%N`
+end=`date +%s`
 runtime=$((end-start))
 echo "codegen time: $runtime" >> deepdsl-java/$result_file
 #cd ..
-start=`date +%s.%N`
+start=`date +%s`
 mvn -Plinux64 clean install -DskipTests 2>&1 >/dev/null
-end=`date +%s.%N`
+end=`date +%s`
 runtime=$((end-start))
 echo "maven re-build(after codegen) time: $runtime" >> deepdsl-java/$result_file
 cd deepdsl-java
@@ -71,9 +71,9 @@ echo "-------------------------------------------------------"
 echo "test set 2: with only enableMemoryCache disabled" >> $result_file
 sed -i -- 's/JCudaTensor.enableMemoryCache();/\/\/JCudaTensor.enableMemoryCache();/g' src/main/java/deepdsl/gen/*java
 cd ..
-start=`date +%s.%N`
+start=`date +%s`
 mvn -Plinux64 clean install -DskipTests 2>&1 >/dev/null
-end=`date +%s.%N`
+end=`date +%s`
 runtime=$((end-start))
 echo "maven re-build(after enableMemoryCache is disabled) time: $runtime" >> deepdsl-java/$result_file
 cd deepdsl-java
@@ -85,20 +85,20 @@ echo "-------------------------------------------------------"
 echo "test set 3: with both enableMemoryCache and enableWorkspaceCache disabled" >> $result_file
 sed -i -- 's/JCudaTensor.enableWorkspaceCache();/\/\/JCudaTensor.enableWorkspaceCache();/g' src/main/java/deepdsl/gen/*java
 cd ..
-start=`date +%s.%N`
+start=`date +%s`
 mvn -Plinux64 clean install -DskipTests 2>&1 >/dev/null
-end=`date +%s.%N`
+end=`date +%s`
 runtime=$((end-start))
 echo "maven re-build(after both enableMemoryCache and enableWorkspaceCache are disabled) time: $runtime" >> deepdsl-java/$result_file
 cd deepdsl-java
 do_test $freq $package test3
 
 #restore cache
-start=`date +%s.%N`
-end=`date +%s.%N`
+start=`date +%s`
+sed -i -- 's,//JCudaTensor,JCudaTensor,g' src/main/java/deepdsl/gen/*java
+end=`date +%s`
 runtime=$((end-start))
 echo "code restore(re-enabled both enableMemoryCache and enableWorkspaceCache)  time: $runtime" >> deepdsl-java/$result_file
-sed -i -- 's,//JCudaTensor,JCudaTensor,g' src/main/java/deepdsl/gen/*java
 
 cd ..
 echo "Finish testing"
