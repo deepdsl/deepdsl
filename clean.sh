0cc34097b4458312ecae2736e24eeeba4b682c10
pir-dsl <matthewhuang2004@gmail.com>
2017-09-15 02:48:11 -0500

    Fixed a couple issues

---
+++ clean.sh
@@ -0,0 +1,26 @@
+#!/bin/bash 
+
+rm deepdsl-java/*txt
+rm deepdsl-java/*log
+rm deepdsl-java/src/main/java/deepdsl/gen/*_infer.java
+git checkout deepdsl-java/src/main/java/deepdsl/gen/*java
+git checkout deepdsl-java/src/main/java/deepdsl/gen/*_infer.java
+git checkout deepdsl-java/src/main/java/deepdsl/gen/Lenet.java
+git checkout deepdsl-java/src/main/java/deepdsl/gen/Lenet_tanh.java
+rm deepdsl-java/src/main/java/deepdsl/gen/Alexnet128.java
+rm deepdsl-java/src/main/java/deepdsl/gen/Alexnet256.java
+rm deepdsl-java/src/main/java/deepdsl/gen/Googlenet128.java
+rm deepdsl-java/src/main/java/deepdsl/gen/Googlenet256.java
+rm deepdsl-java/src/main/java/deepdsl/gen/Overfeat128.java
+rm deepdsl-java/src/main/java/deepdsl/gen/Overfeat256.java
+rm deepdsl-java/src/main/java/deepdsl/gen/Resnet32.java
+rm deepdsl-java/src/main/java/deepdsl/gen/Resnet64.java
+rm deepdsl-java/src/main/java/deepdsl/gen/Vgg64.java
+rm deepdsl-java/src/main/java/deepdsl/gen/alexnet128/*
+rm deepdsl-java/src/main/java/deepdsl/gen/alexnet256/*
+rm deepdsl-java/src/main/java/deepdsl/gen/googlenet128/*
+rm deepdsl-java/src/main/java/deepdsl/gen/googlenet256/*
+rm deepdsl-java/src/main/java/deepdsl/gen/resnet32/*
+rm deepdsl-java/src/main/java/deepdsl/gen/resnet64/*
+rm deepdsl-java/src/main/java/deepdsl/gen/vgg64/*
+ 
--- combo_test.sh
+++ combo_test.sh
@@ -11,11 +11,7 @@ do_test() {
         do
         #test one network at a time
         nvidia-smi --query-gpu=memory.used --format=csv >> $network-$test_num.txt -lms $1 &
-        start=`date +%s`
-        mvn -Plinux64 exec:java -Dexec.mainClass="$2.$network"
-        end=`date +%s`
-        runtime=$((end-start))
-        echo "$network execution time: $runtime" >> deepdsl-java/$result_file
+        { time mvn -Plinux64 exec:java -Dexec.mainClass="$2.$network" ; } 2>&1 | grep real | sed -En "s/real\s*(.+)/\1/p" | xargs echo "$network execution time: " >> $result_file
         
         #to use both $network and $1, need 1. use double quate and put backslash in front of $1 to allow
         #variable interpolation
@@ -29,35 +25,24 @@ do_test() {
 clean() {
        for network in Alexnet128 Alexnet256 Googlenet128 Googlenet256 Overfeat128 Overfeat256 Vgg64 Resnet32 Resnet64
        do
-       	rm deepdsl-java/src/main/java/deepdsl/gen/$network.java 
-       	rm deepdsl-java/src/main/java/deepdsl/gen/$network\_infer.java 
-       	rm deepdsl-java/$network-test1.txt
-       	rm deepdsl-java/$network-test2.txt
-       	rm deepdsl-java/$network-test3.txt
+        rm deepdsl-java/src/main/java/deepdsl/gen/$network.java
+        rm deepdsl-java/src/main/java/deepdsl/gen/$network\_infer.java
+        rm deepdsl-java/$network-test1.txt
+        rm deepdsl-java/$network-test2.txt
+        rm deepdsl-java/$network-test3.txt
+	rm deepdsl-java/*.log
        done
 }
 echo "Start testing"
 echo "Start test time $(date)" >> deepdsl-java/$result_file
 #assuming start from deepdsl root folder
 #Step 1: generate the code and compile
-start=`date +%s`
-mvn -Plinux64 clean install -DskipTests 2>&1 >/dev/null
-end=`date +%s`
-runtime=$((end-start))
-echo "maven build time: $runtime" >> deepdsl-java/$result_file
+{ time mvn -Plinux64 clean install -DskipTests; } 2>&1 | grep real | sed -En "s/real\s*(.+)/\1/p" | xargs echo "maven build time: " >> deepdsl-java/$result_file
 #cd deepdsl-java
 #mvn -Plinux64 test -Dtest=TestNetwork#testAll
-start=`date +%s`
-mvn -Plinux64 test -Dtest=TestNetwork 2>&1 >/dev/null
-end=`date +%s`
-runtime=$((end-start))
-echo "codegen time: $runtime" >> deepdsl-java/$result_file
+{ time mvn -Plinux64 test -Dtest=TestNetwork; } 2>&1 | grep real | sed -En "s/real\s*(.+)/\1/p" | xargs echo "codegen time: " >> deepdsl-java/$result_file
 #cd ..
-start=`date +%s`
-mvn -Plinux64 clean install -DskipTests 2>&1 >/dev/null
-end=`date +%s`
-runtime=$((end-start))
-echo "maven re-build(after codegen) time: $runtime" >> deepdsl-java/$result_file
+{ time mvn -Plinux64 clean install -DskipTests; } 2>&1 | grep real | sed -En "s/real\s*(.+)/\1/p" | xargs echo "maven re-build(after codegen) time: " >> deepdsl-java/$result_file
 cd deepdsl-java
 
 #Step 2: Run test suite 1
@@ -71,11 +56,7 @@ echo "-------------------------------------------------------"
 echo "test set 2: with only enableMemoryCache disabled" >> $result_file
 sed -i -- 's/JCudaTensor.enableMemoryCache();/\/\/JCudaTensor.enableMemoryCache();/g' src/main/java/deepdsl/gen/*java
 cd ..
-start=`date +%s`
-mvn -Plinux64 clean install -DskipTests 2>&1 >/dev/null
-end=`date +%s`
-runtime=$((end-start))
-echo "maven re-build(after enableMemoryCache is disabled) time: $runtime" >> deepdsl-java/$result_file
+{ time mvn -Plinux64 clean install -DskipTests; } 2>&1 | grep real | sed -En "s/real\s*(.+)/\1/p" | xargs echo "maven re-build(after enableMemoryCache is disabled) time: " >> deepdsl-java/$result_file
 cd deepdsl-java
 do_test $freq $package test2
 sleep 10
@@ -85,22 +66,14 @@ echo "-------------------------------------------------------"
 echo "test set 3: with both enableMemoryCache and enableWorkspaceCache disabled" >> $result_file
 sed -i -- 's/JCudaTensor.enableWorkspaceCache();/\/\/JCudaTensor.enableWorkspaceCache();/g' src/main/java/deepdsl/gen/*java
 cd ..
-start=`date +%s`
-mvn -Plinux64 clean install -DskipTests 2>&1 >/dev/null
-end=`date +%s`
-runtime=$((end-start))
-echo "maven re-build(after both enableMemoryCache and enableWorkspaceCache are disabled) time: $runtime" >> deepdsl-java/$result_file
+{ time mvn -Plinux64 clean install -DskipTests; } 2>&1 | grep real | sed -En "s/real\s*(.+)/\1/p" | xargs echo "maven re-build(after both enableMemoryCache and enableWorkspaceCache are disabled) time: " >> deepdsl-java/$result_file
 cd deepdsl-java
 do_test $freq $package test3
 
 #restore cache
-start=`date +%s`
-sed -i -- 's,//JCudaTensor,JCudaTensor,g' src/main/java/deepdsl/gen/*java
-end=`date +%s`
-runtime=$((end-start))
-echo "code restore(re-enabled both enableMemoryCache and enableWorkspaceCache)  time: $runtime" >> deepdsl-java/$result_file
-
+{ time sed -i -- 's,//JCudaTensor,JCudaTensor,g' src/main/java/deepdsl/gen/*java; } 2>&1 | grep real | sed -En "s/real\s*(.+)/\1/p" | xargs echo "code restore(re-enabled both enableMemoryCache and enableWorkspaceCache)  time: " >> $result_file
 cd ..
 echo "Finish testing"
 echo "End test time $(date)" >> deepdsl-java/$result_file
 clean
+
